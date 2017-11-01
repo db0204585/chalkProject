0b05e712ed16b34bdbcba8783b7402569f5902f3
David Blacksher <db0204585@otc.edu>
2017-11-01 09:49:15 -0500

    final commit

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 //November 1, 2017
 //Markley
 
-var _ = require('lodash');
+var lodash = require('lodash');
 var chalk = require(`chalk`);
+var jsonfile = require('jsonfile')
+
+var file = 'data.json'
+jsonfile.readFile(file, function(err, people) {
+
+    lodash.forEach(people.items[0], function(value) {
+        console.log(value);
+
+    });
+
+    lodash.forEach(people.items[1], function(value) {
+        console.log(value);
+
+    });
+});
+
 
